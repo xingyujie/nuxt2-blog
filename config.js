d1bb41d1622fd2f16e7527a00fbb8366acd1c301
xingyujie <39870096+xingyujie@users.noreply.github.com>
2022-04-02 20:54:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: 'yunyuyuan', // github账户名
+  title: '小邢的实验室', // 网站标题
+  nickName: "邢大帅", // 昵称
+  githubName: 'xingyujie', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
-  domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  domain: 'http://blog.xingyujie.top', // rss域名
+  SEO_description: '小邢的实验室博客，物联网、程序、Linux、软件的入坑记录与技术分享',
+  SEO_keywords: '小邢的实验室\'s blog,小邢的实验室的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc" // cloudflare的webanalyze
 }
